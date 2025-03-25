ada03ef807377297e6f3cb6287010d21804f4d3b
Stefan Boca <stefan.r.boca@gmail.com>
2025-03-24 18:37:35 -0700

    feat: F# parser

--- src/parser.rs
+++ src/parser.rs
@@ -39,6 +39,7 @@ pub fn parse_filetype(
         "cpp" => Some(parse_with_lexer(CppToken::lexer(text), initial_state)),
         "csharp" => Some(parse_with_lexer(CSharpToken::lexer(text), initial_state)),
         "dart" => Some(parse_with_lexer(DartToken::lexer(text), initial_state)),
+        "fsharp" => Some(parse_with_lexer(FSharpToken::lexer(text), initial_state)),
         "go" => Some(parse_with_lexer(GoToken::lexer(text), initial_state)),
         "haskell" => Some(parse_with_lexer(HaskellToken::lexer(text), initial_state)),
         "java" => Some(parse_with_lexer(JavaToken::lexer(text), initial_state)),
